26a9259db630f9f3335d482526e2fd956983db57
Melinda Allen <msal71396@edu.ucl.dk>
2026-04-10 09:15:11 +0200

    API og APP

---
+++ OverlapssystemAPI/Controllers/PNMedicinController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OverlapssytemApplication.Interfaces;
+using OverlapssytemApplication.Services;
+
+namespace OverlapssystemAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PNMedicinController : ControllerBase
+    {
+        private readonly IPNMedicinService _pNMedicinService;
+
+        public PNMedicinController (IPNMedicinService pnMedicinService)
+        {
+            _pNMedicinService = pnMedicinService;
+        }
+    }
+}
---
+++ OverlapssystemAPI/Controllers/ShoppingController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OverlapssystemAPI.Controllers
+{
+    public class ShoppingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
