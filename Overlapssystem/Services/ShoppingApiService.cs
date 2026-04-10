d25268341354e34d75dfaba6b33738c9351b9aa1
Melinda Allen <msal71396@edu.ucl.dk>
2026-04-10 12:19:53 +0200

    ShoppingApiService

---
+++ Overlapssystem/Services/ShoppingApiService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OverlapssystemDomain.Entities;
+using OverlapssystemShared;
+
+namespace Overlapssystem.Services
+{
+    public class ShoppingApiService
+    {
+        private readonly HttpClient _http;
+        public ShoppingApiService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        //Hent
+        public async Task<List<ShoppingModel>> GetShoppingByResidentId(int residentId)
+        {
+            return await _http.GetFromJsonAsync<List<ShoppingModel>>($"api/Shopping/Shopping/{residentId}");
+        }
+        //Tilføj
+        public async Task SaveNewShopping(ShoppingModel shoppingModel)
+        {
+            await _http.PostAsJsonAsync("api/Shopping/TilføjShopping", shoppingModel);
+        }
+
+        //Update
+        public async Task UpdateShopping(int shoppingId, ShoppingModel shoppingModel)
+        {
+            await _http.PutAsJsonAsync($"api/Shopping/{shoppingId}", shoppingModel);
+        }
+
+        //Delete
+        public async Task DeleteShopping(int shoppingId)
+        {
+            await _http.DeleteAsync($"api/Shopping/{shoppingId}");
+        }
+
+    }
+}
