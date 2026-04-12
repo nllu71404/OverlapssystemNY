c3f8d09e21f98b314a7597ce78f9725599a14cbf
Ninna Lundberg <nllu71404@edu.ucl.dk>
2026-04-12 14:17:24 +0200

    Rettelser i afhængigheder i DepartmentService

--- OverlapssytemApplication/Services/DepartmentService.cs
+++ OverlapssytemApplication/Services/DepartmentService.cs
@@ -5,48 +5,49 @@ using System.Text;
 using System.Threading.Tasks;
 using OverlapssystemDomain.Entities;
 using OverlapssytemApplication.Interfaces;
+using OverlapssystemDomain.Interfaces;
 
 namespace OverlapssytemApplication.Services
 {
     public class DepartmentService : IDepartmentService
     {
 
-        private readonly IDepartmentService _departmentService;
+        private readonly IDepartmentRepository _departmentRepository;
 
-        public DepartmentService()
+        public DepartmentService(IDepartmentRepository departmentRepository)
         {
-           
+           _departmentRepository = departmentRepository;
         }
 
         public List<DepartmentModel> Departments { get; private set; } = new();
 
         public async Task<List<DepartmentModel>> GetAllDepartmentsAsync()
         {
-            return Departments = await _departmentService.GetAllDepartmentsAsync();
+            return Departments = await _departmentRepository.GetAllDepartmentsAsync();
         }
 
         public async Task<DepartmentModel> GetDepartmentByIdAsync(int departmentId)
         {
-            return await _departmentService.GetDepartmentByIdAsync(departmentId);
+            return await _departmentRepository.GetDepartmentByIdAsync(departmentId);
         }
 
         public async Task<DepartmentModel> GetDepartmentByNameAsync(string departmentName)
         {
-            return await _departmentService.GetDepartmentByNameAsync(departmentName);
+            return await _departmentRepository.GetDepartmentByNameAsync(departmentName);
         }
 
         public async Task SaveNewDepartmentAsync(DepartmentModel department)
         {
-            await _departmentService.SaveNewDepartmentAsync(department);
+            await _departmentRepository.SaveNewDepartmentAsync(department);
         }
 
         public async Task DeleteDepartmentAsync(int departmentId)
         {
-            await _departmentService.DeleteDepartmentAsync(departmentId);
+            await _departmentRepository.DeleteDepartmentAsync(departmentId);
         }
         public async Task UpdateDepartmentAsync(DepartmentModel department)
         {
-             await _departmentService.UpdateDepartmentAsync(department);
+             await _departmentRepository.UpdateDepartmentAsync(department);
         }
     }
 }
