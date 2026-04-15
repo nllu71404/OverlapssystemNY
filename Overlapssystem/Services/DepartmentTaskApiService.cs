18d5a8506b384d5fe8ede0f71e9b0020c3d92f91
Melinda Allen <msal71396@edu.ucl.dk>
2026-04-15 10:26:29 +0200

    Updates

--- Overlapssystem/Services/DepartmentTaskApiService.cs
+++ Overlapssystem/Services/DepartmentTaskApiService.cs
@@ -13,30 +13,30 @@ namespace Overlapssystem.Services
         //Hent
         public async Task<List<DepartmentTaskModel>> GetAllDepartmentTask()
         {
-            return await _http.GetFromJsonAsync<List<DepartmentTaskModel>>("api/DepartmentTask/HentDepartmentsTask");
+            return await _http.GetFromJsonAsync<List<DepartmentTaskModel>>("api/DepartmentTask/HentDepartmentsTasks");
         }
         //Hent på ID
         public async Task<List<DepartmentTaskModel>> GetDepartmentTaskById(int? id)
         {
-            return await _http.GetFromJsonAsync<List<DepartmentTaskModel>>($"api/DepartmentTask/HentDepartmentstaskId/{id}");
+            return await _http.GetFromJsonAsync<List<DepartmentTaskModel>>($"api/DepartmentTask/HentDepartmentTasksID/{id}");
         }
 
         //Tilføj
         public async Task CreateDepartmentTask(DepartmentTaskModel departmentTaskModel)
         {
-            await _http.PostAsJsonAsync("api/DepartmentTask/OpretDepartmentTask", departmentTaskModel);
+            await _http.PostAsJsonAsync("api/DepartmentTask/TilføjDepartmentTask", departmentTaskModel);
         }
 
         //Update
         public async Task UpdateDepartmentTask(int id, DepartmentTaskModel departmentTaskModel)
         {
-            await _http.PutAsJsonAsync($"api/DepartmentTask/UpdateDepartmentTask/{id}", departmentTaskModel);
+            await _http.PutAsJsonAsync($"api/DepartmentTask/{id}", departmentTaskModel);
         }
 
         //Delete
         public async Task DeleteDepartmentTask(int id)
         {
-            await _http.DeleteAsync($"api/DepartmentTask/SletDepartmentTask/{id}");
+            await _http.DeleteAsync($"api/DepartmentTask/{id}");
         }
     }
 }
