51d426ef93ed288ffc5d94e503e6deebef178ae7
Melinda Allen <msal71396@edu.ucl.dk>
2026-04-15 09:48:40 +0200

    DepartmentTask

---
+++ OverlapssystemAPI/Controllers/DepartmentTaskController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OverlapssystemDomain.Entities;
+using OverlapssytemApplication.Interfaces;
+using OverlapssytemApplication.Services;
+
+namespace OverlapssystemAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentTaskController : ControllerBase
+    {
+        private readonly IDepartmentTaskService _departmentTaskService;
+        public DepartmentTaskController(IDepartmentTaskService departmentTaskService)
+        {
+            _departmentTaskService = departmentTaskService;
+        }
+
+        //Hent alle
+
+
+        //[HttpGet("HenterResident")]
+        //public async Task<ActionResult> GetResidents()
+        //{
+        //    //Skal kalde LoadResidentAsync og returnerer resultatet
+        //    var residents = await _residentServices.LoadResidentsAsync();
+        //    return Ok(residents);
+        //}
+        //Hent departmentTask på id
+
+        //Tilføj
+
+        //Update
+
+        //Delete
+    }
+}
