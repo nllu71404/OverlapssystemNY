448e12f87dae21ffc31622f7223585cc535d20dc
Mia Madsen <mia_madsen92@hotmail.com>
2026-04-20 13:19:22 +0200

    Oprettet EmployeePhonemodel, Serviceklasse, og repository

---
+++ OverlapssystemDomain/Interfaces/IEmployeePhoneRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OverlapssystemDomain.Entities;
+
+namespace OverlapssystemDomain.Interfaces
+{
+    public interface IEmployeePhoneRepository
+    {
+        Task<List<EmployeePhoneModel>> GetAllEmployeePhoneNumbersAsync();
+        Task<EmployeePhoneModel> GetEmployeePhoneByIdAsync(int employeePhoneId);
+        Task<int> SaveNewEmployeePhoneAsync(EmployeePhoneModel employeePhone);
+        Task DeleteEmployeePhoneAsync(int employeePhoneId);
+        Task UpdateEmployeePhoneAsync(EmployeePhoneModel employeePhone);
+    }
+}
