37813512e28ef8ea2e354280e63c68baf1ad59e5
Mia Madsen <mia_madsen92@hotmail.com>
2026-04-21 17:22:56 +0200

    Tilføjet API, API-serviceklasse

---
+++ Overlapssystem/Services/EmployeePhoneApiService.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http.Json;
+using OverlapssystemDomain.Entities;
+
+namespace Overlapssystem.Services
+{
+    public class EmployeePhoneApiService
+    {
+        private readonly HttpClient _http;
+        public EmployeePhoneApiService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        //Hent Alle
+        public async Task<List<EmployeePhoneModel>> GetAllEmployeePhoneNumbers()
+        {
+            return await _http.GetFromJsonAsync<List<EmployeePhoneModel>>("api/EmployeePhone/HentAlleEmployeePhones");
+        }
+
+        //Hent på Id
+        public async Task<EmployeePhoneModel> GetEmployeePhoneById(int employeePhoneId)
+        {
+            return await _http.GetFromJsonAsync<EmployeePhoneModel>($"api/EmployeePhone/HentEmployeePhoneById/{employeePhoneId}");
+        }
+
+        //Tilføj
+        public async Task SaveNewEmployeePhone(EmployeePhoneModel employeePhoneModel)
+        {
+            await _http.PostAsJsonAsync("api/EmployeePhone/TilføjEmployeePhone", employeePhoneModel);
+        }
+
+        //Update
+        public async Task UpdateEmployeePhone(int employeePhoneId, EmployeePhoneModel employeePhoneModel)
+        {
+            await _http.PutAsJsonAsync($"api/EmployeePhone/OpdaterEmployeePhone/{employeePhoneId}", employeePhoneModel);
+        }
+
+        //Delete
+        public async Task DeleteEmployeePhone(int employeePhoneId)
+        {
+            await _http.DeleteAsync($"api/EmployeePhone/SletEmployeePhone/{employeePhoneId}");
+        }
+
+    }
+}
---
+++ OverlapssystemAPI/Controllers/EmployeePhoneController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OverlapssystemDomain.Entities;
+using OverlapssytemApplication.Services;
+using OverlapssytemApplication.Interfaces;
+
+
+namespace OverlapssystemAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EmployeePhoneController : ControllerBase
+    {
+        private readonly IEmployeePhoneService _employeePhoneService;
+
+        public EmployeePhoneController(IEmployeePhoneService employeePhoneService)
+        {
+            _employeePhoneService = employeePhoneService;
+        }
+
+        //Hent alle medarbejdertelefonnumre
+        [HttpGet("HentAlleEmployeePhones")]
+        public async Task<ActionResult> GetAllEmployeePhoneNumbersAsync()
+        {
+            var employeePhones = await _employeePhoneService.GetAllEmployeePhoneNumbersAsync();
+            return Ok(employeePhones.Value);
+        }
+
+        //Hent medarbejdertelefon på id
+        [HttpGet("HentEmployeePhoneById/{employeePhoneId}")]
+        public async Task<ActionResult> GetEmployeePhoneByIdAsync(int employeePhoneId)
+        {
+            var employeePhone = await _employeePhoneService.GetEmployeePhoneByIdAsync(employeePhoneId);
+            if (employeePhone == null)
+            {
+                return NotFound();
+            }
+            return Ok(employeePhone.Value);
+        }
+
+        //Tilføj/Gem et medarbejdertelefonnummer
+        [HttpPost("TilføjEmployeePhone")]
+        public async Task<ActionResult> SaveNewEmployeePhoneAsync([FromBody] EmployeePhoneModel employeePhoneModel)
+        {
+            var result = await _employeePhoneService.SaveNewEmployeePhoneAsync(employeePhoneModel);
+            return Ok(result.Value);
+        }
+
+        //Update et medarbejdertelefonnummer
+        [HttpPut("OpdaterEmployeePhone/{employeePhoneId}")]
+        public async Task<ActionResult> UpdateEmployeePhoneAsync(int employeePhoneId, [FromBody] EmployeePhoneModel employeePhoneModel)
+        {
+            await _employeePhoneService.UpdateEmployeePhoneAsync(employeePhoneModel);
+            return Ok(employeePhoneModel);
+        }
+
+        //Delete et medarbejdertelefonnummer
+        [HttpDelete("SletEmployeePhone/{employeePhoneId}")]
+        public async Task<ActionResult> DeleteEmployeePhoneAsync(int employeePhoneId)
+        {
+            await _employeePhoneService.DeleteEmployeePhoneAsync(employeePhoneId);
+            return Ok(employeePhoneId);
+        }
+
+
+    }
+}
